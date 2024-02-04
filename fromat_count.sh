f7e224dc254c07d5c0f323e32e4e4975c234daff
paopaooux <146108555+paopaooux@users.noreply.github.com>
2024-02-04 15:37:02 +0800

    Update fromat_count.sh

--- fromat_count.sh
+++ fromat_count.sh
@@ -2,7 +2,7 @@
 
 # 格式化
 clang-format -i $(find  -type f -name *.h)
-clang-format -i $(find  -type f -name *.cpp)
+clang-format -i $(find  ./ stress-test/ -type f -name *.cpp)
 
 # 统计代码行数
 cloc --git $(git branch --show-current)
